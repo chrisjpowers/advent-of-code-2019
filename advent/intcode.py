669d3aa072e4e9b595254dbd71e5af84907fe69c
Chris Powers <chrisjpowers@gmail.com>
2019-12-10 12:05:21 -0600

    Updated Intcode domain language

--- advent/intcode.py
+++ advent/intcode.py
@@ -1,13 +1,13 @@
 class Intcode:
-    def __init__(self, codes, index=0, done=False):
-        self.index = index
-        self.codes = codes
+    def __init__(self, memory, pointer=0, done=False):
+        self.pointer = pointer
+        self.memory = memory
         self.done = done
     
-    def replace(self, index, value):
-        codes = self.codes.copy()
-        codes[index] = value
-        return Intcode(codes, index=self.index, done=self.done)
+    def replace(self, pointer, value):
+        memory = self.memory.copy()
+        memory[pointer] = value
+        return Intcode(memory, pointer=self.pointer, done=self.done)
 
     def compute(self):
         intcode = self
@@ -16,43 +16,47 @@ class Intcode:
         return intcode
 
     def run(self):
-        codes = self.codes.copy()
-        command_code = self.codes[self.index]
+        memory = self.memory.copy()
+        command_code = self.memory[self.pointer]
+        next_pointer = self.pointer + self.instruction_length
         if command_code == 1:
-            codes[self.target_index] = self.left_operand + self.right_operand
-            return Intcode(codes, index=self.index + 4)
+            memory[self.target_address] = self.parameter_1 + self.parameter_2
+            return Intcode(memory, pointer=next_pointer)
         elif command_code == 2:
-            codes[self.target_index] = self.left_operand * self.right_operand
-            return Intcode(codes, index=self.index + 4)
+            memory[self.target_address] = self.parameter_1 * self.parameter_2
+            return Intcode(memory, pointer=next_pointer)
         elif command_code == 99:
-            return Intcode(codes, index=self.index + 4, done=True)
-
-    def add(self):
-        if self.codes[self.index] == 1:
-            codes = self.codes.copy()
-            return codes
+            return Intcode(memory, pointer=next_pointer, done=True)
 
     @property
-    def left_operand(self):
-        left_index = self.codes[self.index + 1]
-        return self.codes[left_index]
+    def instruction_length(self):
+        return 4
+
+    @property
+    def parameter_1(self):
+        return self.value_represented_by_address(self.pointer + 1)
     
     @property
-    def right_operand(self):
-        right_index = self.codes[self.index + 2]
-        return self.codes[right_index]
+    def parameter_2(self):
+        return self.value_represented_by_address(self.pointer + 2)
     
     @property
-    def target_index(self):
-        return self.codes[self.index + 3]
+    def target_address(self):
+        return self.value_at_address(self.pointer + 3)
+    
+    def value_represented_by_address(self, address):
+        return self.memory[self.value_at_address(address)]
+
+    def value_at_address(self, address):
+        return self.memory[address]
         
     def __eq__(self, other):
         return (
-            self.codes == other.codes and
-            self.index == other.index and
+            self.memory == other.memory and
+            self.pointer == other.pointer and
             self.done == other.done
         )
 
     def __repr__(self):
-        codes = ','.join([str(c) for c in self.codes])
-        return f"<Intcode done={self.done} index={self.index} codes={codes}>"
+        memory = ','.join([str(c) for c in self.memory])
+        return f"<Intcode done={self.done} pointer={self.pointer} memory={memory}>"
--- test/intcode_test.py
+++ test/intcode_test.py
@@ -3,34 +3,34 @@ from advent import Intcode
 
 
 class TestIntcode(unittest.TestCase):
-    def test_stores_codes_and_index(self):
-        ic = Intcode([1, 2, 3], index=4)
-        self.assertEqual(ic.index, 4)
-        self.assertEqual(ic.codes, [1, 2, 3])
+    def test_stores_memory_and_pointer(self):
+        ic = Intcode([1, 2, 3], pointer=4)
+        self.assertEqual(ic.pointer, 4)
+        self.assertEqual(ic.memory, [1, 2, 3])
     
     def test_simple_addition(self):
         ic     = Intcode([1, 0, 5, 0, 99, 8])
-        target = Intcode([9, 0, 5, 0, 99, 8], index=8, done=True)
+        target = Intcode([9, 0, 5, 0, 99, 8], pointer=8, done=True)
         self.assertEqual(ic.compute(), target)
 
     def test_simple_multiplication(self):
         ic     = Intcode([2,4,4,5,99,0])
-        target = Intcode([2,4,4,5,99,9801], index=8, done=True)
+        target = Intcode([2,4,4,5,99,9801], pointer=8, done=True)
         self.assertEqual(ic.compute(), target)
 
     def test_simple_done(self):
         ic     = Intcode([99, 1, 2, 3])
-        target = Intcode([99, 1, 2, 3], index=4, done=True)
+        target = Intcode([99, 1, 2, 3], pointer=4, done=True)
         self.assertEqual(ic.compute(), target)
     
     def test_compute(self):
         ic     = Intcode([1,1,1,4,99,5,6,0,99])
-        target = Intcode([30,1,1,4,2,5,6,0,99], index=12, done=True)
+        target = Intcode([30,1,1,4,2,5,6,0,99], pointer=12, done=True)
         self.assertEqual(ic.compute(), target)
     
     def test_early_stop(self):
         ic     = Intcode([1,9,10,3,2,3,11,0,99,30,40,50])
-        target = Intcode([3500,9,10,70,2,3,11,0,99,30,40,50], index=12, done=True)
+        target = Intcode([3500,9,10,70,2,3,11,0,99,30,40,50], pointer=12, done=True)
         self.assertEqual(ic.compute(), target)
     
     def test_replace(self):
