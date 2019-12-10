afb314b6fae5f9596e35a275ca8cb0fc722b6230
Chris Powers <chrisjpowers@gmail.com>
2019-12-09 21:21:46 -0600

    Cleanup

--- scripts/star_01.py
+++ scripts/star_01.py
@@ -1,14 +1,18 @@
 from advent import Ship, ShipModule
 
-def day_01():
+
+def star_01():
     s = Ship()
     [s.add_module(ShipModule(mass=m)) for m in masses()]
     print(s.fuel_for_launch())
 
+
 def masses():
-    with open("day_01_data.txt") as f:
+    with open("star_01_data.txt") as f:
         contents = f.read()
         return [int(line) for line in contents.split("\n")]
 
+
 if __name__== "__main__":
-    day_01()
+    star_01()
+
